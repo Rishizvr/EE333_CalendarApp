48c1df7f9a618cd483729bdb551abaf4fb3582a1
Rishi Zaveri <72662032+Rishizvr@users.noreply.github.com>
2021-10-19 20:50:26 -0500

    Update CalendarApp.java

--- CalendarApp/CalendarApp.java
+++ CalendarApp/CalendarApp.java
@@ -1,8 +1,7 @@
 /*
  * File: Calendar.java
  * Author: Rishi Zaveri sage0@uab.edu
- * Assignment:  sage0-p5 - EE333 Fall 2019
- * Vers: 1.0.0 10/29/2019 rz - initial coding
+
  *
  * Credits: N/A
  */
